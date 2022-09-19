926836e95c8353f8b8570b51905eab81839d96c3
clopez11224 <christopherlopez000@gmail.com>
2022-09-19 13:23:24 -0400

    Refactored code

--- landingPage/landingPage.js
+++ landingPage/landingPage.js
@@ -1,23 +1,21 @@
 let str = "Fast & Furious";
-
-const fetchData = () => {
-    //we check if the input is more than one word, if so we translate the title to url code format.
-    let newStr = ""
-    if (str.split(" ").length > 1) {
-        str = str.split(" ");
-        console.log(str)
-        for (let i = 0; i < str.length -1; i++) {
-            newStr += (str[i] + "%20");
-        }
-        newStr += str[str.length - 1];
+//we check if the input is more than one word, if so we translate the title to url code format by adding %20(space).
+let newStr = ""
+if (str.split(" ").length > 1) {
+    str = str.split(" ");
+    for (let i = 0; i < str.length - 1; i++) {
+        newStr += (str[i] + "%20");
     }
-    str = newStr
-    console.log(str)
+    newStr += str[str.length - 1];
+}
+str = newStr
+
+// Fetch possible movie title results
+const fetchData = () => {
     fetch(`https://api.watchmode.com/v1/search/?apiKey=g7hJI9XVOqZCYV3Uv0RLX8evqs1y6KrU96V2B3n5&search_field=name&search_value=${str}`)
         .then(data => data.json())
         .then(response => {
             console.log(response)
         })
 }
-// https://api.watchmode.com/v1/search/?apiKey=YOUR_API_KEY&search_field=name&search_value=Ed%20Wood'
 fetchData();
