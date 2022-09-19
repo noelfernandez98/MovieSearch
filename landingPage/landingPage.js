0acb94bbcd1df4ec50a1e2df0a36686a40c1fd70
clopez11224 <christopherlopez000@gmail.com>
2022-09-19 13:16:37 -0400

    Fetched movie title feature

--- landingPage/landingPage.js
+++ landingPage/landingPage.js
@@ -1,22 +1,23 @@
-// let url = "https://api.watchmode.com/v1/?apiKey=g7hJI9XVOqZCYV3Uv0RLX8evqs1y6KrU96V2B3n5"
-let str = "TokyoDrift";
+let str = "Fast & Furious";
 
 const fetchData = () => {
-    url = `https://api.watchmode.com/v1/search?apiKey=g7hJI9XVOqZCYV3Uv0RLX8evqs1y6KrU96V2B3n5&search_field=name&search_value=${str}`;
     //we check if the input is more than one word, if so we translate the title to url code format.
+    let newStr = ""
     if (str.split(" ").length > 1) {
         str = str.split(" ");
+        console.log(str)
         for (let i = 0; i < str.length -1; i++) {
-            url += (str[i] + "%20");
+            newStr += (str[i] + "%20");
         }
-        url += str[str.length - 1];
+        newStr += str[str.length - 1];
     }
-    console.log(url)
-    fetch(url)
+    str = newStr
+    console.log(str)
+    fetch(`https://api.watchmode.com/v1/search/?apiKey=g7hJI9XVOqZCYV3Uv0RLX8evqs1y6KrU96V2B3n5&search_field=name&search_value=${str}`)
         .then(data => data.json())
         .then(response => {
             console.log(response)
         })
 }
-https://api.watchmode.com/v1/search/?apiKey=YOUR_API_KEY&search_field=name&search_value=Ed%20Wood'
+// https://api.watchmode.com/v1/search/?apiKey=YOUR_API_KEY&search_field=name&search_value=Ed%20Wood'
 fetchData();
