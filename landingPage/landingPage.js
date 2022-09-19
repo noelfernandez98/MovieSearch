b14314f5c094601ea73ab05ff74ef37865c8ff15
Noel Fernandez <noelfernandez98@gmail.com>
2022-09-19 19:24:35 -0400

    created a submit form. And used input values to fetch data. (peer prog w/ chris)

--- landingPage/landingPage.js
+++ landingPage/landingPage.js
@@ -1,4 +1,8 @@
-let str = "Godzilla";
+let submitBtn = document.getElementById("submitBtn");
+let input = document.getElementById("search");
+let str = "tokyoDrift";
+
+
 //we check if the input is more than one word, if so we translate the title to url code format by adding %20(space).
 let newStr = ""
 if (str.split(" ").length > 1) {
@@ -11,18 +15,25 @@ if (str.split(" ").length > 1) {
 if(newStr.length > 0) str = newStr
 
 // Fetch possible movie title results
-const fetchData = () => {
-    fetch(`https://api.watchmode.com/v1/search/?apiKey=g7hJI9XVOqZCYV3Uv0RLX8evqs1y6KrU96V2B3n5&search_field=name&search_value=${str}`)
+const fetchData = (string) => {
+    fetch(`https://api.watchmode.com/v1/autocomplete-search/?apiKey=g7hJI9XVOqZCYV3Uv0RLX8evqs1y6KrU96V2B3n5&search_field=name&search_value=${string}`)
         .then(data => data.json())
         .then(response => {
-            console.log(response)
-            console.log(response.title_results) //array of data results(id: imdb_id: name: resultType tmdb_id  tmdb_type type year
-            console.log(response.title_results[0])
-            console.log(response.title_results[1])
-            for(let i = 0; i <= 10 && i < response.title_results.length; i++) {
-               console.log(response.title_results[i].name) // Console.log all title name results
+            console.log(response.results); // Entire response array with results
+
+            // Console.log all title name results up to 10 
+            let length = 10 < response.results.length ? 10 : response.results.length;
+            for (let i = 0; i <= length ; i++) {
+                console.log(response.results[i].name);
             }
         })
 }
 
-fetchData();
+submitBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    str = input.value;
+    console.log(input.value);
+    fetchData(str);
+})
+
+// fetchData();
