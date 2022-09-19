de85bd1d8af1c8332fd3cb3eff5e0be78d7fb986
Noel Fernandez <noelfernandez98@gmail.com>
2022-09-19 11:56:28 -0400

    created a function to fetch search input

--- landingPage/landingPage.js
+++ landingPage/landingPage.js
@@ -1,12 +1,22 @@
-let url = "https://api.watchmode.com/v1/status/?apiKey=g7hJI9XVOqZCYV3Uv0RLX8evqs1y6KrU96V2B3n5"
+// let url = "https://api.watchmode.com/v1/?apiKey=g7hJI9XVOqZCYV3Uv0RLX8evqs1y6KrU96V2B3n5"
+let str = "TokyoDrift";
 
 const fetchData = () => {
+    url = `https://api.watchmode.com/v1/search?apiKey=g7hJI9XVOqZCYV3Uv0RLX8evqs1y6KrU96V2B3n5&search_field=name&search_value=${str}`;
+    //we check if the input is more than one word, if so we translate the title to url code format.
+    if (str.split(" ").length > 1) {
+        str = str.split(" ");
+        for (let i = 0; i < str.length -1; i++) {
+            url += (str[i] + "%20");
+        }
+        url += str[str.length - 1];
+    }
+    console.log(url)
     fetch(url)
         .then(data => data.json())
         .then(response => {
-            response.console.log(response)
+            console.log(response)
         })
 }
-
-
+https://api.watchmode.com/v1/search/?apiKey=YOUR_API_KEY&search_field=name&search_value=Ed%20Wood'
 fetchData();
