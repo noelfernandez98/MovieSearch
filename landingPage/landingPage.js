c3eb4b79b947246ef2996c554381061359e0c373
clopez11224 <christopherlopez000@gmail.com>
2022-09-19 15:54:48 -0400

    Explored data. Fixed bug that reassigned variable(added condition)

--- landingPage/landingPage.js
+++ landingPage/landingPage.js
@@ -1,4 +1,4 @@
-let str = "Fast & Furious";
+let str = "Godzilla";
 //we check if the input is more than one word, if so we translate the title to url code format by adding %20(space).
 let newStr = ""
 if (str.split(" ").length > 1) {
@@ -8,7 +8,7 @@ if (str.split(" ").length > 1) {
     }
     newStr += str[str.length - 1];
 }
-str = newStr
+if(newStr.length > 0) str = newStr
 
 // Fetch possible movie title results
 const fetchData = () => {
@@ -16,6 +16,13 @@ const fetchData = () => {
         .then(data => data.json())
         .then(response => {
             console.log(response)
+            console.log(response.title_results) //array of data results(id: imdb_id: name: resultType tmdb_id  tmdb_type type year
+            console.log(response.title_results[0])
+            console.log(response.title_results[1])
+            for(let i = 0; i <= 10 && i < response.title_results.length; i++) {
+               console.log(response.title_results[i].name) // Console.log all title name results
+            }
         })
 }
+
 fetchData();
