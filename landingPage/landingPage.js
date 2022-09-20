b83e9f97e88f64721d3c21fc88257953c099e12c
clopez11224 <christopherlopez000@gmail.com>
2022-09-20 10:28:09 -0400

    Stash commit

--- landingPage/landingPage.js
+++ landingPage/landingPage.js
@@ -1,5 +1,7 @@
+// DOM elements
 let submitBtn = document.getElementById("submitBtn");
 let input = document.getElementById("search");
+let searchResults = document.getElementById("search-results")
 
 //This function checks the given inputValue and converts it to URL code format if spaces are present
 const strWithUrlFormat = (inputValue) => {
@@ -25,12 +27,26 @@ const fetchData = (string) => {
 
             // Console.log all title name results up to 10 
             let length = 10 < response.results.length ? 10 : response.results.length;
-            for (let i = 0; i <= length; i++) {
+            for (let i = 0; i < length; i++) {
+                let titleName = document.createElement("li")
+                titleName.innerText = response.results[i].name
+                searchResults.appendChild(titleName)
+                let id =
+
                 console.log(response.results[i].name);
             }
         })
 }
 
+const fetchTitleDetails = (titleId) => {
+    fetch(`https://api.watchmode.com/v1/title/${titleId}/details/?apiKey=g7hJI9XVOqZCYV3Uv0RLX8evqs1y6KrU96V2B3n5`)
+    .then(data => data.json())
+    .then(response => {
+        console.log(response)
+    })
+}
+
+// Event Listener for Submit Button
 submitBtn.addEventListener("click", (e) => {
     e.preventDefault();
     str = strWithUrlFormat(input.value)
@@ -39,5 +55,3 @@ submitBtn.addEventListener("click", (e) => {
 })
 
 
-
-// fetchData();
